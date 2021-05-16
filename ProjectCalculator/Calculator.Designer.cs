bc526d073a5505d44ef0cfecf9d0f3cba3e40657
Rashid Cruz <cruzrashidl27@gmail.com>
2021-05-16 20:23:07 +0800

    initial commit

---
+++ ProjectCalculator/Calculator.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace ProjectCalculator
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.number_0 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // number_0
+            // 
+            this.number_0.Location = new System.Drawing.Point(12, 280);
+            this.number_0.Name = "number_0";
+            this.number_0.Size = new System.Drawing.Size(75, 43);
+            this.number_0.TabIndex = 0;
+            this.number_0.Text = "0";
+            this.number_0.UseVisualStyleBackColor = true;
+            this.number_0.Click += new System.EventHandler(this.number_0_Click);
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 335);
+            this.Controls.Add(this.number_0);
+            this.Name = "Calculator";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button number_0;
+    }
+}
+

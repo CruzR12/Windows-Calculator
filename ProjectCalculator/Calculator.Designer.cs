3ddeab0fb8b2be2a1423c32e5ede7ed34f525a17
Rashid Cruz <cruzrashidl27@gmail.com>
2021-06-28 10:38:25 +0800

    added sign button

--- ProjectCalculator/Calculator.Designer.cs
+++ ProjectCalculator/Calculator.Designer.cs
@@ -46,16 +46,17 @@
             this.equals_button = new System.Windows.Forms.Button();
             this.clear_button = new System.Windows.Forms.Button();
             this.clear_entry_button = new System.Windows.Forms.Button();
-            this.result_box = new System.Windows.Forms.TextBox();
             this.result_label = new System.Windows.Forms.Label();
+            this.result_box = new System.Windows.Forms.TextBox();
+            this.sign_button = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // number_0
             // 
             this.number_0.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.number_0.Location = new System.Drawing.Point(12, 253);
+            this.number_0.Location = new System.Drawing.Point(83, 396);
             this.number_0.Name = "number_0";
-            this.number_0.Size = new System.Drawing.Size(108, 49);
+            this.number_0.Size = new System.Drawing.Size(70, 50);
             this.number_0.TabIndex = 0;
             this.number_0.TabStop = false;
             this.number_0.Text = "0";
@@ -65,9 +66,9 @@
             // number_1
             // 
             this.number_1.Font = new System.Drawing.Font("Segoe UI", 15F);
-            this.number_1.Location = new System.Drawing.Point(12, 198);
+            this.number_1.Location = new System.Drawing.Point(7, 340);
             this.number_1.Name = "number_1";
-            this.number_1.Size = new System.Drawing.Size(51, 49);
+            this.number_1.Size = new System.Drawing.Size(70, 50);
             this.number_1.TabIndex = 3;
             this.number_1.TabStop = false;
             this.number_1.Text = "1";
@@ -77,9 +78,9 @@
             // number_2
             // 
             this.number_2.Font = new System.Drawing.Font("Segoe UI", 15F);
-            this.number_2.Location = new System.Drawing.Point(69, 198);
+            this.number_2.Location = new System.Drawing.Point(83, 340);
             this.number_2.Name = "number_2";
-            this.number_2.Size = new System.Drawing.Size(51, 49);
+            this.number_2.Size = new System.Drawing.Size(70, 50);
             this.number_2.TabIndex = 4;
             this.number_2.TabStop = false;
             this.number_2.Text = "2";
@@ -89,9 +90,9 @@
             // number_3
             // 
             this.number_3.Font = new System.Drawing.Font("Segoe UI", 15F);
-            this.number_3.Location = new System.Drawing.Point(126, 198);
+            this.number_3.Location = new System.Drawing.Point(159, 340);
             this.number_3.Name = "number_3";
-            this.number_3.Size = new System.Drawing.Size(51, 49);
+            this.number_3.Size = new System.Drawing.Size(70, 50);
             this.number_3.TabIndex = 5;
             this.number_3.TabStop = false;
             this.number_3.Text = "3";
@@ -101,9 +102,9 @@
             // number_4
             // 
             this.number_4.Font = new System.Drawing.Font("Segoe UI", 15F);
-            this.number_4.Location = new System.Drawing.Point(12, 143);
+            this.number_4.Location = new System.Drawing.Point(7, 284);
             this.number_4.Name = "number_4";
-            this.number_4.Size = new System.Drawing.Size(51, 49);
+            this.number_4.Size = new System.Drawing.Size(70, 50);
             this.number_4.TabIndex = 6;
             this.number_4.TabStop = false;
             this.number_4.Text = "4";
@@ -113,9 +114,9 @@
             // number_5
             // 
             this.number_5.Font = new System.Drawing.Font("Segoe UI", 15F);
-            this.number_5.Location = new System.Drawing.Point(69, 143);
+            this.number_5.Location = new System.Drawing.Point(83, 284);
             this.number_5.Name = "number_5";
-            this.number_5.Size = new System.Drawing.Size(51, 49);
+            this.number_5.Size = new System.Drawing.Size(70, 50);
             this.number_5.TabIndex = 7;
             this.number_5.TabStop = false;
             this.number_5.Text = "5";
@@ -125,9 +126,9 @@
             // number_6
             // 
             this.number_6.Font = new System.Drawing.Font("Segoe UI", 15F);
-            this.number_6.Location = new System.Drawing.Point(126, 143);
+            this.number_6.Location = new System.Drawing.Point(159, 284);
             this.number_6.Name = "number_6";
-            this.number_6.Size = new System.Drawing.Size(51, 49);
+            this.number_6.Size = new System.Drawing.Size(70, 50);
             this.number_6.TabIndex = 8;
             this.number_6.TabStop = false;
             this.number_6.Text = "6";
@@ -137,9 +138,9 @@
             // number_7
             // 
             this.number_7.Font = new System.Drawing.Font("Segoe UI", 15F);
-            this.number_7.Location = new System.Drawing.Point(12, 88);
+            this.number_7.Location = new System.Drawing.Point(7, 228);
             this.number_7.Name = "number_7";
-            this.number_7.Size = new System.Drawing.Size(51, 49);
+            this.number_7.Size = new System.Drawing.Size(70, 50);
             this.number_7.TabIndex = 9;
             this.number_7.TabStop = false;
             this.number_7.Text = "7";
@@ -149,9 +150,9 @@
             // number_8
             // 
             this.number_8.Font = new System.Drawing.Font("Segoe UI", 15F);
-            this.number_8.Location = new System.Drawing.Point(69, 88);
+            this.number_8.Location = new System.Drawing.Point(83, 228);
             this.number_8.Name = "number_8";
-            this.number_8.Size = new System.Drawing.Size(51, 49);
+            this.number_8.Size = new System.Drawing.Size(70, 50);
             this.number_8.TabIndex = 10;
             this.number_8.TabStop = false;
             this.number_8.Text = "8";
@@ -161,9 +162,9 @@
             // number_9
             // 
             this.number_9.Font = new System.Drawing.Font("Segoe UI", 15F);
-            this.number_9.Location = new System.Drawing.Point(126, 88);
+            this.number_9.Location = new System.Drawing.Point(159, 228);
             this.number_9.Name = "number_9";
-            this.number_9.Size = new System.Drawing.Size(51, 49);
+            this.number_9.Size = new System.Drawing.Size(70, 50);
             this.number_9.TabIndex = 11;
             this.number_9.TabStop = false;
             this.number_9.Text = "9";
@@ -173,9 +174,9 @@
             // decimal_button
             // 
             this.decimal_button.Font = new System.Drawing.Font("Segoe UI", 16F);
-            this.decimal_button.Location = new System.Drawing.Point(126, 253);
+            this.decimal_button.Location = new System.Drawing.Point(159, 396);
             this.decimal_button.Name = "decimal_button";
-            this.decimal_button.Size = new System.Drawing.Size(51, 49);
+            this.decimal_button.Size = new System.Drawing.Size(70, 50);
             this.decimal_button.TabIndex = 12;
             this.decimal_button.TabStop = false;
             this.decimal_button.Text = ".";
@@ -185,9 +186,9 @@
             // division_button
             // 
             this.division_button.Font = new System.Drawing.Font("Segoe UI", 15F);
-            this.division_button.Location = new System.Drawing.Point(183, 88);
+            this.division_button.Location = new System.Drawing.Point(235, 172);
             this.division_button.Name = "division_button";
-            this.division_button.Size = new System.Drawing.Size(51, 49);
+            this.division_button.Size = new System.Drawing.Size(70, 50);
             this.division_button.TabIndex = 13;
             this.division_button.TabStop = false;
             this.division_button.Text = "/";
@@ -197,9 +198,9 @@
             // multiplication_button
             // 
             this.multiplication_button.Font = new System.Drawing.Font("Segoe UI", 15F);
-            this.multiplication_button.Location = new System.Drawing.Point(183, 143);
+            this.multiplication_button.Location = new System.Drawing.Point(235, 228);
             this.multiplication_button.Name = "multiplication_button";
-            this.multiplication_button.Size = new System.Drawing.Size(51, 49);
+            this.multiplication_button.Size = new System.Drawing.Size(70, 50);
             this.multiplication_button.TabIndex = 14;
             this.multiplication_button.TabStop = false;
             this.multiplication_button.Text = "*";
@@ -209,9 +210,9 @@
             // subtraction_button
             // 
             this.subtraction_button.Font = new System.Drawing.Font("Segoe UI", 15F);
-            this.subtraction_button.Location = new System.Drawing.Point(183, 198);
+            this.subtraction_button.Location = new System.Drawing.Point(235, 284);
             this.subtraction_button.Name = "subtraction_button";
-            this.subtraction_button.Size = new System.Drawing.Size(51, 49);
+            this.subtraction_button.Size = new System.Drawing.Size(70, 50);
             this.subtraction_button.TabIndex = 15;
             this.subtraction_button.TabStop = false;
             this.subtraction_button.Text = "-";
@@ -221,9 +222,9 @@
             // addition_button
             // 
             this.addition_button.Font = new System.Drawing.Font("Segoe UI", 15F);
-            this.addition_button.Location = new System.Drawing.Point(183, 253);
+            this.addition_button.Location = new System.Drawing.Point(235, 340);
             this.addition_button.Name = "addition_button";
-            this.addition_button.Size = new System.Drawing.Size(51, 49);
+            this.addition_button.Size = new System.Drawing.Size(70, 50);
             this.addition_button.TabIndex = 16;
             this.addition_button.TabStop = false;
             this.addition_button.Text = "+";
@@ -233,9 +234,9 @@
             // equals_button
             // 
             this.equals_button.Font = new System.Drawing.Font("Segoe UI", 15F);
-            this.equals_button.Location = new System.Drawing.Point(240, 198);
+            this.equals_button.Location = new System.Drawing.Point(235, 396);
             this.equals_button.Name = "equals_button";
-            this.equals_button.Size = new System.Drawing.Size(51, 104);
+            this.equals_button.Size = new System.Drawing.Size(70, 50);
             this.equals_button.TabIndex = 17;
             this.equals_button.Text = "=";
             this.equals_button.UseVisualStyleBackColor = true;
@@ -244,9 +245,9 @@
             // clear_button
             // 
             this.clear_button.Font = new System.Drawing.Font("Segoe UI", 15F);
-            this.clear_button.Location = new System.Drawing.Point(240, 143);
+            this.clear_button.Location = new System.Drawing.Point(159, 116);
             this.clear_button.Name = "clear_button";
-            this.clear_button.Size = new System.Drawing.Size(51, 49);
+            this.clear_button.Size = new System.Drawing.Size(70, 50);
             this.clear_button.TabIndex = 18;
             this.clear_button.TabStop = false;
             this.clear_button.Text = "C";
@@ -256,26 +257,14 @@
             // clear_entry_button
             // 
             this.clear_entry_button.Font = new System.Drawing.Font("Segoe UI", 15F);
-            this.clear_entry_button.Location = new System.Drawing.Point(240, 88);
+            this.clear_entry_button.Location = new System.Drawing.Point(83, 116);
             this.clear_entry_button.Name = "clear_entry_button";
-            this.clear_entry_button.Size = new System.Drawing.Size(51, 49);
+            this.clear_entry_button.Size = new System.Drawing.Size(70, 50);
             this.clear_entry_button.TabIndex = 19;
             this.clear_entry_button.TabStop = false;
             this.clear_entry_button.Text = "CE";
             this.clear_entry_button.UseVisualStyleBackColor = true;
             this.clear_entry_button.Click += new System.EventHandler(this.clear_entry_button_Click);
-            // 
-            // result_box
-            // 
-            this.result_box.Font = new System.Drawing.Font("Segoe UI", 20F);
-            this.result_box.Location = new System.Drawing.Point(12, 23);
-            this.result_box.Multiline = true;
-            this.result_box.Name = "result_box";
-            this.result_box.Size = new System.Drawing.Size(279, 47);
-            this.result_box.TabIndex = 20;
-            this.result_box.TabStop = false;
-            this.result_box.Text = "0";
-            this.result_box.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // result_label
             // 
@@ -288,11 +277,37 @@
             this.result_label.Size = new System.Drawing.Size(0, 15);
             this.result_label.TabIndex = 21;
             // 
+            // result_box
+            // 
+            this.result_box.Enabled = false;
+            this.result_box.Font = new System.Drawing.Font("Segoe UI", 20F);
+            this.result_box.Location = new System.Drawing.Point(7, 25);
+            this.result_box.Multiline = true;
+            this.result_box.Name = "result_box";
+            this.result_box.ReadOnly = true;
+            this.result_box.Size = new System.Drawing.Size(298, 47);
+            this.result_box.TabIndex = 20;
+            this.result_box.TabStop = false;
+            this.result_box.Text = "0";
+            this.result_box.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // sign_button
+            // 
+            this.sign_button.Font = new System.Drawing.Font("Segoe UI", 18F);
+            this.sign_button.Location = new System.Drawing.Point(7, 396);
+            this.sign_button.Name = "sign_button";
+            this.sign_button.Size = new System.Drawing.Size(70, 50);
+            this.sign_button.TabIndex = 22;
+            this.sign_button.TabStop = false;
+            this.sign_button.Text = "±";
+            this.sign_button.UseVisualStyleBackColor = true;
+            // 
             // Calculator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(303, 313);
+            this.ClientSize = new System.Drawing.Size(312, 453);
+            this.Controls.Add(this.sign_button);
             this.Controls.Add(this.result_label);
             this.Controls.Add(this.result_box);
             this.Controls.Add(this.clear_entry_button);
@@ -315,7 +330,6 @@
             this.Controls.Add(this.number_0);
             this.Font = new System.Drawing.Font("Segoe UI", 8.25F);
             this.KeyPreview = true;
-            this.MaximumSize = new System.Drawing.Size(319, 352);
             this.MinimumSize = new System.Drawing.Size(319, 352);
             this.Name = "Calculator";
             this.Text = "Calculator";
@@ -345,8 +359,9 @@
         private System.Windows.Forms.Button equals_button;
         private System.Windows.Forms.Button clear_button;
         private System.Windows.Forms.Button clear_entry_button;
-        private System.Windows.Forms.TextBox result_box;
         private System.Windows.Forms.Label result_label;
+        private System.Windows.Forms.TextBox result_box;
+        private System.Windows.Forms.Button sign_button;
     }
 }
 
